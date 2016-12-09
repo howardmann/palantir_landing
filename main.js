ca55bf1f6525fc791783f3aab839341095d2fe76
howardmann <howardmann27@gmail.com>
2016-12-09 15:00:06 +1100

    still trying to fix this

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ $(document).ready(function(){
 
   var clicked = false;
   $('#hamburger').css('cursor','pointer');
-  $('#hamburger').bind('click', function(){
+  $('#hamburger').bind('click', function(e){
+    e.preventDefault();
     if (!clicked) {
       $('.line-1, .line-2, .line-3').addClass('active');
       $('.drop-down').fadeIn();
