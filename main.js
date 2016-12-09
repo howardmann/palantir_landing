e8646767efe8bb23871834f4cb7974120dee3224
howardmann <howardmann27@gmail.com>
2016-12-09 22:48:54 +1100

    refactored jquery dropdown

--- main.js
+++ main.js
@@ -1,17 +1,24 @@
 $(document).ready(function(){
 
-  var clicked = false;
-  $('#hamburger').css('cursor','pointer');
-  $('#hamburger').bind('click', function(e){
-    e.preventDefault();
-    if (!clicked) {
-      $('.line-1, .line-2, .line-3').addClass('active');
-      $('.drop-down').fadeIn();
-      clicked = true;
-    } else {
-      $('.line-1, .line-2, .line-3').removeClass('active');
-      $('.drop-down').fadeOut();
-      clicked = false;
-    }
+  // var clicked = false;
+  // $('#hamburger').css('cursor','pointer');
+  // $('#hamburger').bind('click', function(e){
+  //   e.preventDefault();
+  //   if (!clicked) {
+  //     $('.line-1, .line-2, .line-3').addClass('active');
+  //     $('.drop-down').fadeIn();
+  //     clicked = true;
+  //   } else {
+  //     $('.line-1, .line-2, .line-3').removeClass('active');
+  //     $('.drop-down').fadeOut();
+  //     clicked = false;
+  //   }
+  // });
+
+  $('#hamburger').on('click', function(){
+    $('.drop-down').fadeToggle('fast');
+    $('.line-1, .line-2, .line-3').toggleClass('active');
   });
+
+
 });
